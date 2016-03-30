7ced0aed254a2f7328fce720c0a748e5e4ae155e
Adam Averay <adam@averay.com>
2016-03-30 22:54:57 +0800

    Bumped to version 0.5

--- jquery.featherbox.js
+++ jquery.featherbox.js
@@ -1,4 +1,4 @@
-/*! Featherbox 0.4 | github.com/adamaveray/featherbox | Adam Averay | MIT */
+/*! Featherbox 0.5 | github.com/adamaveray/featherbox | Adam Averay | MIT */
 ;(function($){
 	var $window	= $(window),
 		$body	= $('body');
